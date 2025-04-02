ff688bee2e9a85165136d909425a8934e9c3f476
Scriptscrypt <tirumaladasa@gmail.com>
2025-04-03 00:39:32 +0530

    fix: metadata

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,30 +1,62 @@
-"use client"
-
 import "./globals.css";
 import React from "react";
 import Layout from "@/components/layout/layout";
-import { SolanaProvider } from "@/providers/SolanaProvider";
-import { getWalletAdapters } from "@/config/wallets";
-import { getRpcEndpoint } from "@/config/rpc";
-import { WalletAdapter, WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { Providers } from "@/providers/Providers";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Hypernova',
+  description: 'The Decentralized Exchange & Launchpad for Sonic SVM',
+  keywords: ['solana', 'hackathon', 'ai', 'Hypernova'],
+  authors: [{ name: 'Hypernova', url: 'https://www.hypernova.fun' }],
+  manifest: '/manifest.json',
+  themeColor: '#000000',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'Hypernova',
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    title: 'Hypernova',
+    description: 'The Decentralized Exchange & Launchpad for Sonic SVM',
+    url: 'https://hypernova.fun',
+    siteName: "Hypernova",
+    images: [
+      {
+        url: '/og.png',
+        width: 1200,
+        height: 630,
+        alt: 'Hypernova',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Hypernova',
+    description: 'The Decentralized Exchange & Launchpad for Sonic SVM',
+    images: ['/og.png'],
+  },
+  metadataBase: new URL('https://hypernova.fun'),
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const network = WalletAdapterNetwork.Devnet;
-  const wallets = getWalletAdapters(network);
-  const endpoint = getRpcEndpoint(network, true); // Set to true to use custom RPC
-
   return (
     <html lang="en">
       <body>
-        <SolanaProvider network={network} wallets={wallets as WalletAdapter[]} endpoint={endpoint}>
+        <Providers>
           <Layout>
             {children}
           </Layout>
-        </SolanaProvider>
+        </Providers>
       </body>
     </html>
   );
